27967cb9bb464f0ae03e0666e1c5b7878336eae5
NinaHerrmann <ninaherrmann@D-3170L10.WIWI.UNI-MUENSTER.DE>
2025-08-04 10:13:07 +0200

    updated cdd in case we want it later

--- plottingscripts/cdd.py
+++ plottingscripts/cdd.py
@@ -14,46 +14,54 @@ def create_critical_difference_diagram(results, name):
     matrix = results_df.pivot(index='dataset_name', columns='classifier_name', values='accuracy')
     test_results = sp.posthoc_conover_friedman(matrix)
     plt.figure(figsize=(10, 2), dpi=100)
-    # plt.title('Critical difference diagram of average score ranks')
     avg_rank = results_df.groupby('dataset_name').accuracy.rank(pct=True).groupby(results_df.classifier_name).mean()
     sp.critical_difference_diagram(avg_rank, test_results)
-    plt.savefig('plottingscripts/figures/' + str(name) +'.png')
-    plt.show()
+    plt.tight_layout()
+    plt.savefig('plottingscripts/figures/cdd/' + str(name) +'.pdf')
 
-results_list_avg_accuracy = []
-results_list_accuracy = []
+datasets = ['california', 'cpu_act', 'fried', 'sulfur', 'superconduct', 'wine']
+model_counters = {'FP': 0, 'Po-MQ': 0, 'Po-QQ':0, 'Pr-MQ': 0, 'Pr-QQ': 0,
+              'SQ': 0, 'Bw-MQ': 0, 'Bw-QQ': 0, 'Bw-SQ': 0}
 
-datasets = ['california', 'fried', 'superconduct', 'wine_quality']
-model_counters = {'Full precision': 0, 'Post minmaxQ': 0, 'Post quantileQ':0, 'Pre minmaxQ': 0, 'Pre quantileQ': 0,
-              'SoftQ': 0, 'HardQ': 0, 'Bitwise softQ': 0, 'Bitwise hardQ': 0}
+bits = [2, 3, 4, 5, 6, 7, 8]
 
-for data in datasets:
-    df = pd.read_csv('results/' + f'{data}' + '_best_models__4bits_20runs.csv', sep=',', index_col=False)
-    print(df)
+if not os.path.exists('plottingscripts/figures/cdd/'):
+    os.makedirs('plottingscripts/figures/cdd/')
+    if not os.path.exists('plottingscripts/figures/cdd/avg/'):
+        os.makedirs('plottingscripts/figures/cdd/avg')
+    if not os.path.exists('plottingscripts/figures/cdd/min/'):
+        os.makedirs('plottingscripts/figures/cdd/min')
 
-    methods = {
-        'Full precision': df['val_loss_mlp'].tolist(),
-        'Post minmaxQ': df['val_loss_hard_post_mlp'].tolist(),
-        'Post quantileQ': df['val_loss_hard_thr_post_mlp'].tolist(),
-        'Pre minmaxQ': df['val_loss_hard_pre_mlp'].tolist(),
-        'Pre quantileQ': df['val_loss_hard_thr_pre_mlp'].tolist(),
-        'SoftQ': df['val_loss_soft_mlp'].tolist(),
-        'HardQ': df['val_loss_soft_hard_mlp'].tolist(),
-        'Bitwise softQ': df['val_loss_soft_comp_mlp'].tolist(),
-        'Bitwise hardQ': df['val_loss_soft_comp_mlp'].tolist()
-    }
+for bit in bits:
+    results_list_avg_accuracy = []
+    results_list_accuracy = []
+    for data in datasets:
+        path = f'results/processed_kFold_results/avg_by_hyper/' + f'{data}.csv'
+        fulldata = pd.read_csv(path, sep=',', index_col=False, on_bad_lines='warn')
+        df = fulldata[fulldata['bits'] == bit]
+        print(df)
+        methods = {
+            'FP': df['FP'].tolist(),
+            'Po-MQ': df['Po-MQ'].tolist(),
+            'Po-QQ': df['Po-QQ'].tolist(),
+            'Pr-MQ': df['Pr-MQ'].tolist(),
+            'Pr-QQ': df['Pr-QQ'].tolist(),
+            'SQ': df['SQ'].tolist(),
+            'Bw-MQ': df['Bw-MQ'].tolist(),
+            'Bw-QQ': df['Bw-QQ'].tolist(),
+            'Bw-SQ': df['Bw-SQ'].tolist()
+        }
 
-    for key in methods:
-        column = methods[key]
-        min_values = min(column)
-        counter = 0
-        meanvalue_acc = 0
-        for value in column:
-            meanvalue_acc += value
-            counter += 1
-        model_counters[key] += counter
-        results_list_accuracy.append({'classifier_name': key, 'dataset_name': data, 'accuracy': min_values})
-        results_list_avg_accuracy.append({'classifier_name': key, 'dataset_name': data, 'accuracy': meanvalue_acc/counter})
-
-create_critical_difference_diagram(results_list_accuracy, 'Accuracy')
-create_critical_difference_diagram(results_list_avg_accuracy, 'Average Accuracy')
+        for key in methods:
+            column = methods[key]
+            min_values = min(column)
+            counter = 0
+            meanvalue_acc = 0
+            for value in column:
+                meanvalue_acc += value
+                counter += 1
+            model_counters[key] += counter
+            results_list_accuracy.append({'classifier_name': key, 'dataset_name': data, 'accuracy': min_values})
+            results_list_avg_accuracy.append({'classifier_name': key, 'dataset_name': data, 'accuracy': meanvalue_acc/counter})
+    create_critical_difference_diagram(results_list_accuracy, f'min/{bit}_MSE')
+    create_critical_difference_diagram(results_list_avg_accuracy, f'avg/{bit}_MSE')
